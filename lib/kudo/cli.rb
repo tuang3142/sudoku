8e7b81dec8186db985190b598805fbe5ed3503ea
Nguyen Viet Tuang <tuan.nguyenviet271@gmail.com>
2021-08-22 16:29:49 +0700

    Bump kudo to 0.2.0

--- lib/kudo/cli.rb
+++ lib/kudo/cli.rb
@@ -13,7 +13,10 @@ module Kudo
     desc "solve INPUT", "Solves the sudoku given in the input file"
     def solve(file_name)
       sudoku = File.read(file_name)
-      Kudo.solve(sudoku)
+
+      puts Kudo.solve(sudoku)
+    rescue StandardError => e
+      puts e
     end
   end
 end
